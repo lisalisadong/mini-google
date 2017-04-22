2085b90ae87c9671c7ff3f77604f71c585ebaa0f
Qingxiao Dong <qingxiao@cis.upenn.edu>
2017-04-22 00:32:50 -0400

    semantic autocomplete

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -16,21 +16,29 @@ class MarkdownEditor extends React.Component {
 
   render() {
     return (
-        <div>
+        <div className="ui grid">
           <div className="row"><br/><br/><br/><br/><br/><br/><br/></div>
-          <div className="col-sm-3"></div>
-          <div className="col-sm-6">
-            <h1 style={{textAlign:"center",color:'crimson',fontFamily:'Cochin',fontWeight:'bold',fontSize: 50}}>Mini Google</h1>
-            <div className="row"><br/></div>
-            <form action="/search" method="get">
-                <input className="form-control" type="search" autoFocus="true" name="query"
-                  onChange={this.handleChange}
-                  defaultValue={this.state.value} />
-            </form>
-            <div className="row"><br/></div>
-            <div
-              dangerouslySetInnerHTML={this.getRawMarkup()}
-            />
+          <div className="row">
+              <div className="four wide column"></div>
+              <div className="eight wide column">
+                <h1 style={{textAlign:"center",color:'crimson',fontFamily:'Cochin',fontWeight:'bold',fontSize: 50}}>Mini Google</h1>
+                <div className="row"><br/></div>
+                <form action="/search" method="get">
+                    <div className="ui fluid category search">
+                        <div className="ui fluid icon input">
+                            <input className="prompt" type="text" autoFocus="true" name="query"
+                              onChange={this.handleChange}
+                              defaultValue={this.state.value} />
+                            <i aria-hidden="true" className="search icon" style={{color:'steelblue'}}></i>
+                        </div>
+                        <div className="results"></div>
+                    </div>
+                </form>
+                <div className="row"><br/></div>
+                <div
+                  dangerouslySetInnerHTML={this.getRawMarkup()}
+                />
+              </div>
           </div>
       </div>
     );
@@ -38,3 +46,34 @@ class MarkdownEditor extends React.Component {
 }
 
 ReactDOM.render(<MarkdownEditor />, document.getElementById('root'));
+
+$('.ui.search')
+  .search({
+    type          : 'category',
+    apiSettings: {
+      onResponse: function(githubResponse) {
+              var
+                response = {
+                  results : {}
+                }
+              ;
+              response.results["Github"] = {
+                name    : "Github",
+                results : []
+              };
+              // translate GitHub API response to work with search
+              $.each(githubResponse.items, function(index, item) {
+                // add result to category
+                response.results["Github"].results.push({
+                  title       : item.name,
+                  description : item.description,
+                  url         : item.html_url
+                });
+              });
+              return response;
+            },
+            url: '//api.github.com/search/repositories?q={query}'
+    },
+    minCharacters : 3
+  })
+;
