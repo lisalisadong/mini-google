104794e16cbbb2c99df3389a465e7e3fc2a0423e
lj1010 <ljue1010@gmail.com>
2017-05-06 00:06:37 -0400

    add input format

--- src/indexer/IndexerReducer.java
+++ src/indexer/IndexerReducer.java
@@ -1,24 +1,27 @@
 package indexer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import com.google.common.collect.Lists;
 
 import utils.Constants;
 
 public class IndexerReducer extends Reducer<Text, InterValue, Text, OutputValue> {
 
 	public void reduce(Text key, Iterable<InterValue> values, Context context) throws IOException, InterruptedException {
-		List<InterValue> valueList = Lists.newArrayList(values);
+		List<InterValue> valueList = new ArrayList<InterValue>();
+		for (InterValue value : values) {
+			valueList.add(new InterValue(value.getDocID(), value.getTf(), value.getTitlePositions(), value.getContentPositions()));
+		}
 		int count = valueList.size();
 		double idf = Math.log((double) count / Constants.TOTAL_DOC_NUM);
 		
 		for (InterValue value : valueList) {
-			System.out.println("word:" + key + "docID: " + value.getDocID());
+//			System.out.println("word:" + key + "docID: " + value.getDocID());
 			OutputValue outputValue = new OutputValue(value.getDocID(), value.getTf(), idf, value.getTitlePositions(), value.getContentPositions());
 			context.write(key, outputValue);
 		}
