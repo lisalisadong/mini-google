6eaf13abd611ef289b52b5f98750656ddc5452b7
lj1010 <ljue1010@gmail.com>
2017-05-06 20:17:29 -0400

    add indexer BD

---
+++ src/indexer/DB/Word.java
@@ -0,0 +1,60 @@
+package indexer.DB;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.sleepycat.persist.model.Entity;
+import com.sleepycat.persist.model.PrimaryKey;
+
+@Entity
+public class Word {
+	
+	@PrimaryKey
+	public String word;
+	private Map<String, Double> tfs; // docID : tf
+	private Map<String, Double> idfs; // docID : idf
+	private Map<String, List<Integer>> titlePos; 
+	private Map<String, List<Integer>> contentPos;
+
+	public Word() { }
+	
+	public Word(String word) {
+		this.word = word;
+		this.tfs = new HashMap<String, Double>();
+		this.idfs = new HashMap<String, Double>();
+		this.titlePos = new HashMap<String, List<Integer>>();
+		this.contentPos = new HashMap<String, List<Integer>>();
+	}
+	
+	public Set<String> getDocs() {
+		return tfs.keySet();
+	}
+	
+	public void addInfo(String id, double tf, double idf) {
+		tfs.put(id, tf);
+		idfs.put(id, idf);
+	}
+	
+	public void addTitlePos(String id, List<Integer> pos) {
+		titlePos.put(id, pos);
+	}
+	
+	public void addContentPos(String id, List<Integer> pos) {
+		contentPos.put(id, pos);
+	}
+	
+	public double getTf(String id) {
+		return tfs.get(id);
+	}
+	
+	public List<Integer> getTitlePos(String id) {
+		return titlePos.get(id);
+	}
+	
+	public List<Integer> getContentPos(String id) {
+		return contentPos.get(id);
+	}
+	
+}
